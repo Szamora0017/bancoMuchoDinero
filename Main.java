ea48e3043a356150a796abc4a1c660db4498febd
CarlosG321 <carlos.salinas27789@ucaldas.edu.co>
2024-04-17 14:16:48 -0500

    bancoMuchoDinero

--- Main.java
+++ Main.java
@@ -4,9 +4,10 @@ public class Main {
         BancoMuchoDinero banco = new BancoMuchoDinero();
 
         // Agregar cuentas de ejemplo
-        banco.agregarCuenta(1, 1000, "Juan Perez", "Ahorros");
-        banco.agregarCuenta(2, 2000, "Maria Lopez", "Corriente");
-        banco.agregarCuenta(3, 1500, "Pedro Ramirez", "Ahorros");
+        banco.agregarCuenta(1, 1500000, "Leo PG", "Ahorros");
+        banco.agregarCuenta(2, 3000000, "Sergio Amaya", "Corriente");
+        banco.agregarCuenta(3, 2000000, "Juan Perez", "Ahorros");
+        banco.agregarCuenta(4, 3500000, "Janfry", "Corriente");
 
         // Mostrar lista de cuentas
         banco.mostrarListaDeCuentas();
